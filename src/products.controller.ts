8d44428245a69f26cf1c2a5a1b965a00168b4a66
Breno Guimaraes <brenoaraujo@outlook.com.br>
2021-09-15 13:37:45 -0300

    feat: add Product model implementation

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -7,31 +7,39 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-
+import { Product } from './product.model';
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('LIV001', 'Livro TDD e BDD na prática', 29.9),
+    new Product('LIV002', 'Livro iniciando com flutter', 39.9),
+    new Product('LIV003', 'Livro IA as a service', 29.9),
+  ];
+
   @Get()
-  getAll(): string {
-    return 'get all products';
+  getAll(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  getOne(@Param() params): string {
-    return `get one product by id: ${params.id}`;
+  getOne(@Param() params): Product {
+    return this.products[0];
   }
 
   @Post()
-  create(@Body() product): string {
-    return 'Created product';
+  create(@Body() product: Product): Product {
+    product.id = Date.now();
+    this.products.push(product);
+    return product;
   }
 
   @Put(':id')
-  update(@Body() updateProductBody): string {
-    return 'Updated product';
+  update(@Body() updateProductBody: Product): Product {
+    return updateProductBody;
   }
 
   @Delete(':id')
-  delete(@Param() params): string {
-    return `delete product with id ${params.id}`;
+  delete(@Param() params): void {
+    this.products.pop();
   }
 }
